0666a40d43c3910820734ec19f516a12f728ea20
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-05-04 11:04:39 +0900

    seed heroku v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,31 @@
 require "open-uri"
-# Ingredient.destroy_all
+Ingredient.destroy_all
 
-# json = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+json = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
 
-# items = JSON.parse(json)
+items = JSON.parse(json)
 
-# items["drinks"].each do |ingredient|
-#   Ingredient.create!(name: ingredient["strIngredient1"])
-# end
+items["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
 
 # use a lot of p to debug the each through the json object
 
-require 'faker'
-Cocktail.destroy_all
+# require 'faker'
+# Cocktail.destroy_all
 
 
-puts "creating..."
-n = 0
-4.times do
-  n += 1
-  cocktail = Cocktail.new(
-    name: Faker::TvShows::BreakingBad.episode
-  )
+# puts "creating..."
+# n = 0
+# 4.times do
+#   n += 1
+#   cocktail = Cocktail.new(
+#     name: Faker::TvShows::BreakingBad.episode
+#   )
 
-  file = File.open("#{n}.jpg")
-  cocktail.photo.attach(io: file, filename: "#{n}.jpg", content_type: 'image/jpg')
-  cocktail.save!
-end
+#   file = File.open("#{n}.jpg")
+#   cocktail.photo.attach(io: file, filename: "#{n}.jpg", content_type: 'image/jpg')
+#   cocktail.save!
+# end
 
-puts "...finished"
-
-
+# puts "...finished"
