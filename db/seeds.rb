6d92b126ca0dfbea20c4fa49538795283ecbeacb
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-05-05 11:02:52 +0900

    seeds heroku minor bug fixing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
 require "open-uri"
-Ingredient.destroy_all
+# Ingredient.destroy_all
+Cocktail.destroy_all
 
-json = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+# json = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
 
-items = JSON.parse(json)
+# items = JSON.parse(json)
 
-items["drinks"].each do |ingredient|
-  Ingredient.create!(name: ingredient["strIngredient1"])
-end
+# items["drinks"].each do |ingredient|
+#   Ingredient.create!(name: ingredient["strIngredient1"])
+# end
 
 # use a lot of p to debug the each through the json object
 
